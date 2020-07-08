6c5ddb4e0a5a7067ae493b5b790a53c508af28e5
Gabriel-Tomaz <jgabrieltomaz2706@gmail.com>
2020-07-08 13:15:57 -0300

    Updates

---
+++ precache-manifest.663dd9b79217b7da7a04de4f0100108a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac13a44f1999ee88b705776e743053d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99d4de02eb572946aaec",
+    "url": "/static/css/main.675be016.chunk.css"
+  },
+  {
+    "revision": "9ca284c407def1b2f9bd",
+    "url": "/static/js/2.49bd9f21.chunk.js"
+  },
+  {
+    "revision": "846e3710ca5ee3f911296f05447fe08c",
+    "url": "/static/js/2.49bd9f21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d4de02eb572946aaec",
+    "url": "/static/js/main.6b20b81b.chunk.js"
+  },
+  {
+    "revision": "19f280c0656ebcc06934",
+    "url": "/static/js/runtime-main.f0843700.js"
+  },
+  {
+    "revision": "e97898ddc78260f1d66cdc113f94acec",
+    "url": "/static/media/portfolio.e97898dd.jpeg"
+  },
+  {
+    "revision": "db8f62fd6c12528ae9e304af10e7dffb",
+    "url": "/static/media/profile.db8f62fd.jpg"
+  }
+]);
